71324db930bb02e3a21753b62414bf2db6fd8ee0
JoseCastano1984 <j.albertocast@gmail.com>
2025-04-12 02:46:15 +0100

    first commit - form throwing error

--- assets/mainjs.js
+++ assets/mainjs.js
@@ -133,7 +133,7 @@ $('#contactFormCustomerDetails').on('submit', function(event) {
     formData.append('template_id', 'template_1bly4l6');
     formData.append('user_id', 'NwRE7fbZdGQeHfDDK');
  
-    $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
+    $.ajax('http://localhost:5500/send-form', {
         type: 'POST',
         data: formData,
         contentType: false, // auto-detection
